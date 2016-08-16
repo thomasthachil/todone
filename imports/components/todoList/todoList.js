59c1a5d43d1dc2756eab0eedfae5060a8037ef63
Thomas Thachil <github@thomasthachil.com>
2016-08-16 00:47:27 -0400

    hide completed button

--- imports/components/todoList/todoList.js
+++ imports/components/todoList/todoList.js
@@ -7,47 +7,65 @@ class TodoListCtrl {
     constructor($scope) {
         $scope.viewModel(this);
 
+        this.hideCompleted = false;
+
         this.helpers({
             tasks() {
+                const selector = {};
+
+                // If hide completed is checked, filter tasks
+                if (this.getReactively('hideCompleted')) {
+                    selector.checked = {
+                        $ne: true
+                    };
+                }
+
                 // Show newest tasks at the top
-                return Tasks.find({}, {
+                return Tasks.find(selector, {
                     sort: {
                         createdAt: -1
                     }
                 });
-            }
-        })
-    }
-
-    addTask(newTask) {
-        // Insert a task into the collection
-        Tasks.insert({
-            text: newTask,
-            createdAt: new Date
-        });
-
-        // Clear form
-        this.newTask = '';
-    }
-
-    setChecked(task) {
-        // Set the checked property to the opposite of its current value
-        Tasks.update(task._id, {
-            $set: {
-                checked: !task.checked
             },
+            +      incompleteCount() {
+                +        return Tasks.find({
+                    +          checked: {
+                        +            $ne: true
+                        +          }
+                        +        }).count();
+                    }
+                })
+            }
+
+            addTask(newTask) {
+                // Insert a task into the collection
+                Tasks.insert({
+                    text: newTask,
+                    createdAt: new Date
+                });
+
+                // Clear form
+                this.newTask = '';
+            }
+
+            setChecked(task) {
+                // Set the checked property to the opposite of its current value
+                Tasks.update(task._id, {
+                    $set: {
+                        checked: !task.checked
+                    },
+                });
+            }
+
+            removeTask(task) {
+                Tasks.remove(task._id);
+            }
+        }
+
+        export default angular.module('todoList', [
+            angularMeteor
+        ])
+        .component('todoList', {
+            templateUrl: 'imports/components/todoList/todoList.html',
+            controller: ['$scope', TodoListCtrl]
         });
-    }
-
-    removeTask(task) {
-        Tasks.remove(task._id);
-    }
-}
-
-export default angular.module('todoList', [
-    angularMeteor
-])
-.component('todoList', {
-    templateUrl: 'imports/components/todoList/todoList.html',
-    controller: ['$scope', TodoListCtrl]
-});
